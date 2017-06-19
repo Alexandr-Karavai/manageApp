71809f4e166253b2c88ce4d64d2635e954672609
karavai <karavai96-2010@mail.ru>
2017-06-19 23:00:10 +0300

    add routing

--- app/workers/workers-routing.module.ts
+++ app/workers/workers-routing.module.ts
@@ -1,12 +1,18 @@
-import { NgModule }             from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule }                 from '@angular/core';
+import { RouterModule, Routes }     from '@angular/router';
 
-import { WorkerListComponent }    from './worker-list.component';
-import { WorkerDetailComponent }  from './worker-detail.component';
+import { WorkerListComponent }      from './worker-list/worker-list.component';
+import { WorkerDetailComponent }    from './worker-detail/worker-detail.component';
+import { WorkerEditComponent }      from './edit/worker-edit.component';
+import { NewWorkerComponent }       from './new-worker/new-worker.component';
+import { LoginFormComponent }       from '../login-form/login-form.component';
 
 const workersRoutes: Routes = [
+  { path: 'login',  component: LoginFormComponent },
   { path: 'workers',  component: WorkerListComponent },
-  { path: 'worker/:id', component: WorkerDetailComponent }
+  { path: 'workers/new',  component: NewWorkerComponent },
+  { path: 'worker/:id', component: WorkerDetailComponent },
+  { path: 'worker/edit/:id', component: WorkerEditComponent }
 ];
 
 @NgModule({
